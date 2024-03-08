461744ab639d27e93cf2493c3b8746e2bfb1f246
scorpix06 <alban@cipre.com>
2024-03-08 11:54:39 +0100

    update

--- linode.psm1
+++ linode.psm1
@@ -1,10 +1,10 @@
-# Paramètres de l'API Linode
+# On recupere la clé API dans la variable d'environnement "LTOKEN" pour ne pas avoir d'informations sensible dans le code
 $ApiKey = [System.Environment]::GetEnvironmentVariable("LTOKEN", "User")
-
 # URL de l'API Linode
 $LinodeAPI = "https://api.linode.com/v4"
 
-# Création d'un serveur Linode
+
+# Fonction  qui utilise l'API de Linode pour créer une instance serveur
 function CreateLinodeServer {
     param(
         [string]$Region,
@@ -15,9 +15,8 @@ function CreateLinodeServer {
     )
 
     try {
-        # Création du corps de la requête
 
-        
+        # Création du corps de la requête
         $Body = @{
             region = $Region
             type = $Plan
@@ -26,10 +25,8 @@ function CreateLinodeServer {
             root_pass = $RootPassword
         }
 
-        Write-Host ($Body | ConvertTo-Json)
         # Tentative d'appel à l'API Linode
         $Server = Invoke-WebRequest -Uri "$LinodeAPI/linode/instances" -Method Post -Headers @{Authorization = "Bearer $ApiKey"} -Body ($Body | ConvertTo-Json) -ContentType "application/json" -ErrorAction Stop | ConvertFrom-Json
-        Write-Host $Server.Content
         return $Server
     }
     catch {
@@ -37,14 +34,15 @@ function CreateLinodeServer {
     }
 }
 
+# Fonction  qui utilise l'API de Linode pour récuperer l'état d'un serveur en temps reel
 function GetLinodeStatus {
     param(
-        [string]$Name
+        [string]$Id
     )
     # Tentative d'appel à l'API Linode
-    $Server = Invoke-WebRequest -Uri "$LinodeAPI/linode/instances" -Method Get -Headers @{Authorization = "Bearer $ApiKey"} -Body ($Body | ConvertTo-Json) -ContentType "application/json" -ErrorAction Stop | ConvertFrom-Json
-    Write-Host $Server.Content
-    return $Server
+    $Request = Invoke-WebRequest -Uri "$LinodeAPI/linode/instances/$Id" -Method Get -Headers @{Authorization = "Bearer $ApiKey"} -ContentType "application/json" -ErrorAction Stop | ConvertFrom-Json
+    return $Request
 
 }
+
 
--- main.ps1
+++ main.ps1
@@ -1,6 +1,20 @@
 Import-Module -Name ./linode.psm1
 Import-Module -Name ./telegram.psm1
 Import-Module -Name ./logging.psm1
+Import-Module -Name powershell-yaml
+
+# Importation des parametres du script
+try {
+    $Settings = Get-Content -Path "./settings.yml" | ConvertFrom-Yaml 
+    $DefaultPass = $Settings.default_password
+    $DefaultServerName = $Settings.default_server_name
+    WriteLog -Message "Importation  du fichier de configuration effectue avec succes"
+    
+}
+catch {
+    WriteLog -Message "Erreur lors de l'importation du fichier de configuration : $($_.Exception.Message)"
+}
+
 
 Add-Type -AssemblyName System.Windows.Forms
 
@@ -15,7 +29,7 @@ $comboBox1 = New-Object System.Windows.Forms.ComboBox
 $comboBox1.Location = New-Object System.Drawing.Point(50, 20)
 $comboBox1.Size = New-Object System.Drawing.Size(200, 20)
 $comboBox1.Text = "Choix de l'OS :"
-$comboBox1.Items.AddRange(@("linode/debian12", "linode/alpine3.19", "linode/ubuntu22.04"))
+$comboBox1.Items.AddRange($Settings.os)
 $form.Controls.Add($comboBox1)
 
 # Création d'une liste déroulante pour les specifications
@@ -23,20 +37,20 @@ $comboBox2 = New-Object System.Windows.Forms.ComboBox
 $comboBox2.Location = New-Object System.Drawing.Point(50, 50)
 $comboBox2.Size = New-Object System.Drawing.Size(200, 20)
 $comboBox2.Text = "Type de serveur"
-$comboBox2.Items.AddRange(@("g6-nanode-1", "g6-standard-1"))
+$comboBox2.Items.AddRange($Settings.plan)
 $form.Controls.Add($comboBox2)
 
 # Création d'un champ de texte pour recuperer le nom de la VM cloud
 $textBox = New-Object System.Windows.Forms.TextBox
 $textBox.Location = New-Object System.Drawing.Point(50,80)
 $textBox.Size = New-Object System.Drawing.Size(200,20)
-$textBox.Text = "Serveur-Name"
+$textBox.Text = "$DefaultServerName"
 $form.Controls.Add($textBox)
 
 $textBox2 = New-Object System.Windows.Forms.TextBox
 $textBox2.Location = New-Object System.Drawing.Point(50,110)
 $textBox2.Size = New-Object System.Drawing.Size(200,20)
-$textBox2.Text = "Root-Password@06"
+$textBox2.Text = "$DefaultPass"
 $form.Controls.Add($textBox2)
 
 
@@ -55,20 +69,33 @@ $button.Add_Click({
     $Name = $($textBox.Text)
     $RootPassword = $($textBox2.Text)
 
-    # Envoi de la requête
+    
     try {
-        CreateLinodeServer -Region "fr-par" -Plan $Plan -Image $Image -Name $Name -RootPassword $RootPassword
-        Send-TelegramMessage -Message "Deploiement du serveur [$Name] en cours"
-        WriteLog -Message "Deploiement du serveur [$Name] en cours (plan: $Plan, Image: $Image)"
 
+        # Envoi de la requête a l'API de Linode pour déployer un serveur
+        $Server = CreateLinodeServer -Region "fr-par" -Plan $Plan -Image $Image -Name $Name -RootPassword $RootPassword
+        $ServerIp = $Server.ipv4
+        $ServerStatus = $Server.status 
+
+        Send-TelegramMessage -Message "Deploiement du serveur [$Name] en cours `n IP : $ServerIp `n Status: $ServerStatus"
+        WriteLog -Message "Deploiement du serveur [$Name] en cours ($Server)"
         $message = "Le serveur est en cours de deploiement, le suivi est envoye sur Telegram`r`n"
         [System.Windows.Forms.MessageBox]::Show($message, "Message")
+
+        while (($Server.status -eq "provisioning") -or ($Server.status -eq "booting")) {
+
+            $Server = GetLinodeStatus -Id $Server.id
+            if ($Server.status -eq "running") {
+                Send-TelegramMessage -Message "Le serveur [$Name] est deploye. Vous pouvez desormais vous y connecter." 
+                Write-Log -Message "Le serveur [$Name] est déployé"  
+            }
+            Start-Sleep -Seconds 9
+        }
         
     } catch {
 
         $message = "Une erreur a eu lieu lors du deploiement du serveur : $($_.Exception.Message)`r`n"
         [System.Windows.Forms.MessageBox]::Show($message, "Message")
-        Write-Host "Erreur lors du deploiement du serveur : $($_.Exception.Message)"
         WriteLog -Message "Une erreur a eu lieu lors du déploiement du serveur ($($_.Exception.Message))"
     }
 
